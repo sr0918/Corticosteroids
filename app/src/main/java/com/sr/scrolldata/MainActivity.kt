e47b3c41463a38f4d0ff90f4f2c84bf2a0f33357
sr0918 <sr0918@gmail.com>
2020-02-03 13:48:58 +0300

    3/02/2020

--- app/src/main/java/com/sr/scrolldata/MainActivity.kt
+++ app/src/main/java/com/sr/scrolldata/MainActivity.kt
@@ -7,10 +7,9 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import java.math.RoundingMode
-import kotlin.math.roundToInt
+
 
 val drugs = arrayOf("SELECT DRUG","Prednisolone","Methylprednisolone","Dexamethasone","Betamethasone","Cortisone","Hydrocortisone","Triamcinolone")
 var drugFromSelect:String = ""
@@ -86,7 +85,7 @@ class MainActivity : AppCompatActivity() {
             }
         }*/
 
-        button.setOnClickListener() {
+        button.setOnClickListener {
 
             if (getMg.text.toString() == "") {
                 Toast.makeText(this, "Please, provide valid amount!", Toast.LENGTH_LONG)
@@ -122,186 +121,208 @@ class MainActivity : AppCompatActivity() {
                 }
                 if (drugFromSelect == "Hydrocotisone") {
                     var PrednisoloneDose =
-                        (dose!! * 1).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 1).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     PrednisoloneDoseTV.text = PrednisoloneDose.toString()
                     var CortisoneDose =
-                        (dose!! * 5).toInt()
+                        (dose * 5).toInt()
                     cortisoneDoseTV.text = CortisoneDose.toString()
                     var betamethasoneDose =
-                        (dose!! * 0.12).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.12).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     betamethasoneDoseTV.text = betamethasoneDose.toString()
                     var dexamethasoneDose =
-                        (dose!! * 0.15).toBigDecimal().setScale(2, RoundingMode.UP)
+                        (dose * 0.15).toBigDecimal().setScale(2, RoundingMode.UP)
                             .toDouble()
                     dexamethasoneDoseTV.text = dexamethasoneDose.toString()
                     var hydrocortisoneDose =
-                        (dose!! * 4).toInt()
+                        (dose * 4).toInt()
                     hydrocortisoneDoseTV.text = hydrocortisoneDose.toString()
                     var triamcinoloneDose =
-                        (dose!! * 0.8).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.8).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     triamcinoloneDoseTV.text = triamcinoloneDose.toString()
                     var methylprednisoloneDose =
-                        (dose!! * 0.8).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.8).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     methylprednisoloneDoseTV.text = methylprednisoloneDose.toString()
                 }
                 if (drugFromSelect == "Prednisolone") {
                     var PrednisoloneDose =
-                        (dose!! * 1).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 1).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     PrednisoloneDoseTV.text = PrednisoloneDose.toString()
                     var CortisoneDose =
-                        (dose!! * 5).toInt()
+                        (dose * 5).toInt()
                     cortisoneDoseTV.text = CortisoneDose.toString()
                     var betamethasoneDose =
-                        (dose!! * 0.12).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.12).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     betamethasoneDoseTV.text = betamethasoneDose.toString()
                     var dexamethasoneDose =
-                        (dose!! * 0.15).toBigDecimal().setScale(2, RoundingMode.UP)
+                        (dose * 0.15).toBigDecimal().setScale(2, RoundingMode.UP)
                             .toDouble()
                     dexamethasoneDoseTV.text = dexamethasoneDose.toString()
                     var hydrocortisoneDose =
-                        (dose!! * 4).toInt()
+                        (dose * 4).toInt()
                     hydrocortisoneDoseTV.text = hydrocortisoneDose.toString()
                     var triamcinoloneDose =
-                        (dose!! * 0.8).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.8).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     triamcinoloneDoseTV.text = triamcinoloneDose.toString()
                     var methylprednisoloneDose =
-                        (dose!! * 0.8).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.8).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     methylprednisoloneDoseTV.text = methylprednisoloneDose.toString()
                 }
                 if (drugFromSelect == "Methylprednisolone") {
                     var PrednisoloneDose =
-                        (dose!! * 1.25).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 1.25).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     PrednisoloneDoseTV.text = PrednisoloneDose.toString()
                     var CortisoneDose =
-                        (dose!! * 6.25).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 6.25).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     cortisoneDoseTV.text = CortisoneDose.toString()
                     var betamethasoneDose =
-                        (dose!! * 0.15).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.15).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     betamethasoneDoseTV.text = betamethasoneDose.toString()
                     var dexamethasoneDose =
-                        (dose!! * 0.1875).toBigDecimal().setScale(2, RoundingMode.UP)
+                        (dose * 0.1875).toBigDecimal().setScale(2, RoundingMode.UP)
                             .toDouble()
                     dexamethasoneDoseTV.text = dexamethasoneDose.toString()
                     var hydrocortisoneDose =
-                        (dose!! * 5).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 5).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     hydrocortisoneDoseTV.text = hydrocortisoneDose.toString()
                     var triamcinoloneDose =
-                        (dose!! * 1).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 1).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     triamcinoloneDoseTV.text = triamcinoloneDose.toString()
                     var methylprednisoloneDose =
-                        (dose!! * 1).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 1).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     methylprednisoloneDoseTV.text = methylprednisoloneDose.toString()
                 }
                 if (drugFromSelect == "Dexamethasone") {
                     var PrednisoloneDose =
-                        (dose!! * 6.66).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 6.66).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     PrednisoloneDoseTV.text = PrednisoloneDose.toString()
                     var CortisoneDose =
-                        (dose!! * 33.34).toInt()
+                        (dose * 33.34).toInt()
                     cortisoneDoseTV.text = CortisoneDose.toString()
                     var betamethasoneDose =
-                        (dose!! * 0.8).toBigDecimal().setScale(1, RoundingMode.HALF_EVEN)
+                        (dose * 0.8).toBigDecimal().setScale(1, RoundingMode.HALF_EVEN)
                             .toDouble()
                     betamethasoneDoseTV.text = betamethasoneDose.toString()
                     var dexamethasoneDose =
-                        (dose!! * 1).toBigDecimal().setScale(2, RoundingMode.UP)
+                        (dose * 1).toBigDecimal().setScale(2, RoundingMode.UP)
                             .toDouble()
                     dexamethasoneDoseTV.text = dexamethasoneDose.toString()
                     var hydrocortisoneDose =
-                        (dose!! * 26.67).toInt()
+                        (dose * 26.67).toInt()
                     hydrocortisoneDoseTV.text = hydrocortisoneDose.toString()
                     var triamcinoloneDose =
-                        (dose!! * 5.33).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 5.33).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     triamcinoloneDoseTV.text = triamcinoloneDose.toString()
                     var methylprednisoloneDose =
-                        (dose!! * 5.33).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 5.33).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     methylprednisoloneDoseTV.text = methylprednisoloneDose.toString()
                 }
                 if (drugFromSelect == "Betamethasone") {
                     var PrednisoloneDose =
-                        (dose!! * 8.33).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 8.33).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     PrednisoloneDoseTV.text = PrednisoloneDose.toString()
                     var CortisoneDose =
-                        (dose!! * 41.67).toInt()
+                        (dose * 41.67).toInt()
                     cortisoneDoseTV.text = CortisoneDose.toString()
                     var betamethasoneDose =
-                        (dose!! * 1).toBigDecimal().setScale(1, RoundingMode.HALF_EVEN)
+                        (dose * 1).toBigDecimal().setScale(1, RoundingMode.HALF_EVEN)
                             .toDouble()
                     betamethasoneDoseTV.text = betamethasoneDose.toString()
                     var dexamethasoneDose =
-                        (dose!! * 1.25).toBigDecimal().setScale(2, RoundingMode.UP)
+                        (dose * 1.25).toBigDecimal().setScale(2, RoundingMode.UP)
                             .toDouble()
                     dexamethasoneDoseTV.text = dexamethasoneDose.toString()
                     var hydrocortisoneDose =
-                        (dose!! * 33.34).toInt()
+                        (dose * 33.34).toInt()
                     hydrocortisoneDoseTV.text = hydrocortisoneDose.toString()
                     var triamcinoloneDose =
-                        (dose!! * 6.66).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 6.66).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     triamcinoloneDoseTV.text = triamcinoloneDose.toString()
                     var methylprednisoloneDose =
-                        (dose!! * 6.66).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 6.66).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     methylprednisoloneDoseTV.text = methylprednisoloneDose.toString()
                 }
                 if (drugFromSelect == "Cortisone") {
                     var PrednisoloneDose =
-                        (dose!! * 0.2).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.2).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     PrednisoloneDoseTV.text = PrednisoloneDose.toString()
                     var CortisoneDose =
-                        (dose!! * 1).toInt()
+                        (dose * 1).toInt()
                     cortisoneDoseTV.text = CortisoneDose.toString()
                     var betamethasoneDose =
-                        (dose!! * 0.024).toBigDecimal().setScale(1, RoundingMode.HALF_EVEN)
+                        (dose * 0.024).toBigDecimal().setScale(1, RoundingMode.HALF_EVEN)
                             .toDouble()
                     betamethasoneDoseTV.text = betamethasoneDose.toString()
                     var dexamethasoneDose =
-                        (dose!! * 0.03).toBigDecimal().setScale(2, RoundingMode.UP)
+                        (dose * 0.03).toBigDecimal().setScale(2, RoundingMode.UP)
                             .toDouble()
                     dexamethasoneDoseTV.text = dexamethasoneDose.toString()
                     var hydrocortisoneDose =
-                        (dose!! * 0.8).toInt()
+                        (dose * 0.8).toInt()
                     hydrocortisoneDoseTV.text = hydrocortisoneDose.toString()
                     var triamcinoloneDose =
-                        (dose!! * 0.16).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.16).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     triamcinoloneDoseTV.text = triamcinoloneDose.toString()
                     var methylprednisoloneDose =
-                        (dose!! * 0.16).toBigDecimal().setScale(1, RoundingMode.UP)
+                        (dose * 0.16).toBigDecimal().setScale(1, RoundingMode.UP)
+                            .toDouble()
+                    methylprednisoloneDoseTV.text = methylprednisoloneDose.toString()
+                }
+                if (drugFromSelect == "Triamcinolone") {
+                    var PrednisoloneDose =
+                        (dose * 1.25).toBigDecimal().setScale(1, RoundingMode.UP)
+                            .toDouble()
+                    PrednisoloneDoseTV.text = PrednisoloneDose.toString()
+                    var CortisoneDose =
+                        (dose * 6.25).toBigDecimal().setScale(1, RoundingMode.UP)
+                            .toDouble()
+                    cortisoneDoseTV.text = CortisoneDose.toString()
+                    var betamethasoneDose =
+                        (dose * 0.15).toBigDecimal().setScale(1, RoundingMode.UP)
+                            .toDouble()
+                    betamethasoneDoseTV.text = betamethasoneDose.toString()
+                    var dexamethasoneDose =
+                        (dose * 0.1875).toBigDecimal().setScale(2, RoundingMode.UP)
+                            .toDouble()
+                    dexamethasoneDoseTV.text = dexamethasoneDose.toString()
+                    var hydrocortisoneDose =
+                        (dose * 5).toBigDecimal().setScale(1, RoundingMode.UP)
+                            .toDouble()
+                    hydrocortisoneDoseTV.text = hydrocortisoneDose.toString()
+                    var triamcinoloneDose =
+                        (dose * 1).toBigDecimal().setScale(1, RoundingMode.UP)
+                            .toDouble()
+                    triamcinoloneDoseTV.text = triamcinoloneDose.toString()
+                    var methylprednisoloneDose =
+                        (dose * 1).toBigDecimal().setScale(1, RoundingMode.UP)
                             .toDouble()
                     methylprednisoloneDoseTV.text = methylprednisoloneDose.toString()
                 }
 
-
             }
         }
-
-
     }
-
-
-    //println(selection)
-
-
 }
 
 
