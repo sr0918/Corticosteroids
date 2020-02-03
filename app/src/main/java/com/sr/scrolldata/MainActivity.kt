0a719c45c8692c0596bea6e0cdec322eaf2f45c8
sr0918 <sr0918@gmail.com>
2020-02-03 18:38:37 +0300

    CorticosteroidCalculator 3/02/2020_final

--- app/src/main/java/com/sr/scrolldata/MainActivity.kt
+++ app/src/main/java/com/sr/scrolldata/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.sr.scrolldata
 
 import android.content.Context
+import android.content.DialogInterface
+import android.content.SharedPreferences
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -29,6 +31,34 @@ class MainActivity : AppCompatActivity() {
         val inputManager:InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.SHOW_FORCED)
         }
+// first-run notification
+        val prefs: SharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE)
+        val firstStart = prefs.getBoolean("firstStart", true)
+        val editor = prefs.edit()
+        if (firstStart) {
+            val builder = android.app.AlertDialog.Builder(this)
+                .setMessage(
+                    "All information contained in and produced by the ObGynCalc is provided for educational purposes only. This information should not be used for the diagnosis or treatment of any health problem or disease. This information is not intended to replace clinical judgment or guide individual patient care in any manner.\n" +
+                            "Remember, duration of drug action should be considered along with the potency.\n " +
+                            "The User is hereby notified that the information contained herein may not meet the user’s needs.\n" +
+                            "The User of this software assumes sole responsibility for any decisions made or actions taken based on the information contained in the application.\n" +
+                            "Neither the author nor any other party involved in the preparation, publication or distribution of the Corticosteroid Calculator shall be liable for any special, consequential, or exemplary damages resulting in whole or part from any User’s use of or reliance upon this system and the information contained within.\n" +
+                            "The publisher and developer disclaim all warranties regarding such information whether express or implied, including any warranty as to the quality, accuracy, currency or suitability of this information for any particular purpose.\n" +
+                            "By using the Corticosteroid Calculator, documentation and/or any software found therein, the User agrees to abide by United States and International copyright laws and all other applicable laws involving copyright.\n"
+                )
+                .setPositiveButton(
+                    "Agree and Proceed",
+                    { dialogInterface: DialogInterface, i: Int ->
+                        dialogInterface.dismiss()
+                        editor.putBoolean("firstStart", false)
+                        editor.apply()
+                    })
+                .setNegativeButton("EXIT", { dialogInterface: DialogInterface, i: Int ->
+                    finish()
+                })
+                .show()
+        }
+
 
         val drugAdapter = ArrayAdapter(this, R.layout.spinner_custom, drugs)
         drugAdapter.setDropDownViewResource(R.layout.custom_spinner_dropdown)
